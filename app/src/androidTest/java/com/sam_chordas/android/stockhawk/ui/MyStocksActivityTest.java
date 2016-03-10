3d0c4c67e4db2fafbe6e78f5172a7a289726714c
Daniel Lehrner <daniel@lehrner.me>
2016-03-10 20:19:06 +0100

    First instrumentation tests, custom view Matcher for Toast

---
+++ app/src/androidTest/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivityTest.java
@@ -0,0 +1,56 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.customMatcher.ToastMatcher;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.core.StringEndsWith.endsWith;
+
+
+public class MyStocksActivityTest {
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MyStocksActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void shouldOpenSymbolSearch() {
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(withText(R.string.symbol_search)).check(matches(isDisplayed()));
+        onView(withText(R.string.content_test)).check(matches(isDisplayed()));
+        onView(allOf(withClassName(endsWith("EditText")), withHint(R.string.input_hint)))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldShowSymbolNotFoundToast() {
+        onView(withId(R.id.fab)).perform(click());
+
+        onView(allOf(withClassName(endsWith("EditText")), withHint(R.string.input_hint)))
+                .perform(typeText("Test"));
+
+        onView(withText("OK")).perform(click());
+
+        onView(withText(R.string.symbol_not_found)).inRoot(new ToastMatcher())
+                .check(matches(isDisplayed()));
+    }
+}
--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyStocksActivity.java
@@ -110,7 +110,7 @@ public class MyStocksActivity extends AppCompatActivity implements LoaderManager
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View v) {
         if (isConnected){
-          new MaterialDialog.Builder(mContext).title(R.string.symbol_search)
+            new MaterialDialog.Builder(mContext).title(R.string.symbol_search)
               .content(R.string.content_test)
               .inputType(InputType.TYPE_CLASS_TEXT)
               .input(R.string.input_hint, R.string.input_prefill, new MaterialDialog.InputCallback() {
