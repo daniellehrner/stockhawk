af85e7d20c5be8b0739613c4268643580fd90751
Daniel Lehrner <daniel@lehrner.me>
2016-03-30 13:34:14 +0200

    Added qidget provider

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/QuoteWidgetProvider.java
@@ -0,0 +1,9 @@
+package com.sam_chordas.android.stockhawk.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Daniel Lehrner
+ */
+public class QuoteWidgetProvider extends AppWidgetProvider {
+}
